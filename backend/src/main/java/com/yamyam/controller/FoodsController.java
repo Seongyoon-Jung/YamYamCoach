893c6e5016504bd54a4b800bfa886937b21ae394
now-eat-krap <916ss@naver.com>
2025-05-20 23:53:43 +0900

    Feat: 식단 업로드 UI완성

--- backend/src/main/java/com/yamyam/controller/FoodsController.java
+++ backend/src/main/java/com/yamyam/controller/FoodsController.java
@@ -11,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yamyam.dto.request.FoodData;
 import com.yamyam.dto.request.FoodRequest;
+import com.yamyam.service.FoodService;
 
 @RestController
 @RequestMapping("/api/foods")
 public class FoodsController {
+	private final FoodService foodService;
+	public FoodsController(FoodService foodService) {
+		this.foodService = foodService;
+	}
+
 
 	@PostMapping("")
 	public ResponseEntity<String> saveFoods(@RequestBody FoodRequest request) {
@@ -22,8 +28,12 @@ public class FoodsController {
 	        System.out.println("날짜: " + item.getDate());
 	        System.out.println("코스: " + item.getCourse());
 	        System.out.println("음식: " + item.getFood());
+	        System.out.println();
 	    }
-
+	    
+	    foodService.saveAllCourseSchedule(request.getDiet());
+	    
+	    
 	    return ResponseEntity.ok("OK");
 	}
 }
