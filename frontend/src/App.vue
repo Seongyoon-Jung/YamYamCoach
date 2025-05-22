14716137c1ec7de3228ea75259037b252bb074f5
now-eat-krap <916ss@naver.com>
2025-05-23 00:41:34 +0900

    delete: 로딩 기능 삭제

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,19 @@
 <template>
-  <div>
-    <!-- ✅ 공통 로딩 스피너 -->
-    <LoadingSpinner v-if="isLoading" />
+  <!-- 일반 사용자 화면: ROLE_ADMIN이 아니거나, 현재 경로가 /admin으로 시작하지 않는 경우 -->
+  <div v-if="!isAdmin || !isAdminRoute" id="app" class="d-flex flex-column min-vh-100">
+    <HeaderComponent />
+    <main class="container bg-light flex-grow-1 px-0">
+      <router-view />
+    </main>
+    <FooterComponent />
+  </div>
 
-    <!-- 일반 사용자 화면 -->
-    <div v-if="!isAdmin || !isAdminRoute" id="app" class="d-flex flex-column min-vh-100">
-      <HeaderComponent />
-      <main class="container bg-light flex-grow-1 px-0">
-        <router-view />
-      </main>
-      <FooterComponent />
-    </div>
-
-    <!-- 관리자 화면 -->
-    <div v-else id="app" class="d-flex flex-column min-vh-100">
-      <AdminHeaderComponent />
-      <main class="bg-light flex-grow-1 pt-5">
-        <router-view />
-      </main>
-    </div>
+  <!-- 관리자 화면: ROLE_ADMIN이면서, /admin 경로인 경우 -->
+  <div v-else id="app" class="d-flex flex-column min-vh-100">
+    <AdminHeaderComponent />
+    <main class="bg-light flex-grow-1 pt-5">
+      <router-view />
+    </main>
   </div>
 </template>
 
@@ -26,19 +21,17 @@
 import HeaderComponent from '@/components/section/HeaderComponent.vue'
 import AdminHeaderComponent from '@/components/section/AdminHeaderComponent.vue'
 import FooterComponent from '@/components/section/FooterComponent.vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { userAccountStore } from '@/store/account'
-import { useLoadingStore } from '@/store/loading'
-import { storeToRefs } from 'pinia'
 import { computed } from 'vue'
-import LoadingSpinner from '@/components/loading/LoadingSpinner.vue'
 
 const route = useRoute()
 const accountStore = userAccountStore()
-const loadingStore = useLoadingStore()
-const { isLoading } = storeToRefs(loadingStore)
 
+// 관리자 여부
 const isAdmin = computed(() => accountStore.role === 'ROLE_ADMIN')
+
+// 현재 경로가 /admin 또는 /admin/로 시작하는지 확인
 const isAdminRoute = computed(() => route.path.startsWith('/admin'))
 </script>
 
--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,39 +1,8 @@
-// plugins/axios.js
 import axios from 'axios'
-import { useLoadingStore } from '@/store/loading'
-import { storeToRefs } from 'pinia'
 
-// Axios 인스턴스 생성
 const instance = axios.create({
   baseURL: 'http://localhost:8080',
   withCredentials: true,
 })
-
-// ✅ 인터셉터 등록
-instance.interceptors.request.use(
-  (config) => {
-    const loadingStore = useLoadingStore()
-    loadingStore.startLoading()
-    return config
-  },
-  (error) => {
-    const loadingStore = useLoadingStore()
-    loadingStore.stopLoading()
-    return Promise.reject(error)
-  },
-)
-
-instance.interceptors.response.use(
-  (response) => {
-    const loadingStore = useLoadingStore()
-    loadingStore.stopLoading()
-    return response
-  },
-  (error) => {
-    const loadingStore = useLoadingStore()
-    loadingStore.stopLoading()
-    return Promise.reject(error)
-  },
-)
 
 export default instance
